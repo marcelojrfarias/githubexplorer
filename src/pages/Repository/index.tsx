03db8ac54ecccad04cd75cd24e35e1fd167656b8
marcelojrfarias <marcelojrfarias@outlook.com>
2020-11-02 18:18:18 -0300

    Conects the Details page to the GitHub api.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Header, RepositoryInfo, Issues } from './styles';
+import api from '../../services/api';
+
 import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  user: {
+    login: string;
+  };
+  html_url: string;
+}
+
 const Repository: React.FunctionComponent = () => {
-  // const { params } = useRouteMatch<RepositoryParams>();
+  const { params } = useRouteMatch<RepositoryParams>();
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
 
   return (
     <>
@@ -22,63 +58,52 @@ const Repository: React.FunctionComponent = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars1.githubusercontent.com/u/12167941?s=460&u=84c7ed5e7f7489703f46f2c4994b25b5761151fc&v=4"
-            alt="Marcelo Farias"
-          />
-          <div>
-            <strong>marcelojrfarias/calengrade</strong>
-            <p>descricao do repositorio</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1888</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository ? (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login || 'login'}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
 
-      <Issues>
-        <Link to="issue">
-          <div>
-            <strong>Problema ao importar aula X</strong>
-            <p>Ao tentar importar a aula...</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="issue">
-          <div>
-            <strong>Problema ao importar aula X</strong>
-            <p>Ao tentar importar a aula...</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="issue">
-          <div>
-            <strong>Problema ao importar aula X</strong>
-            <p>Ao tentar importar a aula...</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link to="issue">
-          <div>
-            <strong>Problema ao importar aula X</strong>
-            <p>Ao tentar importar a aula...</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-      </Issues>
+      {issues ? (
+        <Issues>
+          {issues.map(issue => (
+            <a key={issue.id} href={issue.html_url}>
+              <div>
+                <strong>{issue.title}</strong>
+                <p>{issue.user.login}</p>
+              </div>
+              <FiChevronRight size={20} />
+            </a>
+          ))}
+        </Issues>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
     </>
   );
 };
