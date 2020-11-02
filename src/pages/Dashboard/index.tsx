b35d21e76674dd277daca789265956459a29b94f
marcelojrfarias <marcelojrfarias@outlook.com>
2020-11-02 15:18:32 -0300

    Stylizes the Dashboard page.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/12167941?s=460&u=84c7ed5e7f7489703f46f2c4994b25b5761151fc&v=4"
+            alt="Marcelo Farias"
+          />
+          <div>
+            <strong>Calengrade</strong>
+            <p>
+              Ferramenta para gerar arquivo de calendário (.ics) a partir da
+              grade de aulas (.json).
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/12167941?s=460&u=84c7ed5e7f7489703f46f2c4994b25b5761151fc&v=4"
+            alt="Marcelo Farias"
+          />
+          <div>
+            <strong>Calengrade</strong>
+            <p>
+              Ferramenta para gerar arquivo de calendário (.ics) a partir da
+              grade de aulas (.json).
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/12167941?s=460&u=84c7ed5e7f7489703f46f2c4994b25b5761151fc&v=4"
+            alt="Marcelo Farias"
+          />
+          <div>
+            <strong>Calengrade</strong>
+            <p>
+              Ferramenta para gerar arquivo de calendário (.ics) a partir da
+              grade de aulas (.json).
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
